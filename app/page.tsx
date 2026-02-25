a069852d87497ce272a0e5afb86b121ee3cde7a6
labibkamran <lkamran.bscs23seecs@seecs.edu.pk>
2026-02-25 17:45:19 +0500

    feat: design section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Featured from "@/components/home/Featured";
+import Intro from "@/components/home/Intro";
 
 export default function Home() {
   return (
     <main>
       <Featured />
+      <Intro />
     </main>
   );
 }
-
