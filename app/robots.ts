f81407f14426768cf4e455266dcee422630394a7
labibkamran <lkamran.bscs23seecs@seecs.edu.pk>
2026-03-04 13:06:03 +0500

    Fix static export for robots and sitemap routes

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,15 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
-        sitemap: 'https://codexonsolutions.com/sitemap.xml',
-    };
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://codexonsolutions.com/sitemap.xml',
+  };
 }
